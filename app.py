c80520bc75fe0bcb13b37f7f199230dacb43752e
kyungh103 <kyungh103@naver.com>
2019-08-28 13:53:28 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'hello'
+
+# 1. 주문서 만들고,
+# 2. 해당 주문이 들어왔을때 무엇을 할지 정의
+
+@app.route('/name')
+def name():
+    return '박경희'
+
+#variable
+@app.route('/hello/<name>')
+def hello(name):
+    #return 'hello' + name
+    return f'hello {name}'
+    
+#/square/숫자
+#입력된 숫자를 제곱한 결과를 보여줌
+@app.route('/square/<int:number>')
+def square(number):
+    #return str(int(number) ** 2)
+    return str(number ** 2)
+
+#점심메뉴 추천
+@app.route('/menu')
+def menu():
+    foods = ['바스버거','대우식당','진가와','고갯마루']
+    food = random.choice(foods)
+    return food
+
+#lotto 로또번호 추천
+@app.route('/lotto')
+def lotto():
+    winner = [3,5,12,13,33,39]
+    result = random.sample(range(1,46),6)
+    #result = [3,5,12,43,23,19]
+
+    #만약에 6개가 모두 일치하면 1등
+    #만약 5개가 일치하면 3등
+    #만약 4개가 일치하면 4등
+    #만약 3개가 일치하면 5등    
+    num = "꽝"
+    cnt = 0
+
+    cnt = len(set(winner) & set(result))
+
+    if cnt == 6:
+        num = "1등"
+    elif cnt == 5:
+        num = "3등"
+    elif cnt == 4:
+        num = "4등"
+    elif cnt == 3:
+        num = "5등"
+
+    return str(sorted(result)) +" "+ num
