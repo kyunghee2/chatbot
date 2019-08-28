3c32e3bc1d41583b180c2ccbcad6484408b8a26e
kyungh103 <kyungh103@naver.com>
2019-08-28 15:30:04 +0900

    update app.py & apply dhlottery api

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import random
+import requests
 
 app = Flask(__name__)
 
@@ -37,9 +38,17 @@ def menu():
 #lotto 로또번호 추천
 @app.route('/lotto')
 def lotto():
-    winner = [3,5,12,13,33,39]
+    winner =[]
+    #winner = [3,5,12,13,33,39]
     result = random.sample(range(1,46),6)
     #result = [3,5,12,43,23,19]
+    url = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=873'
+    response = requests.get(url)
+    res_dict = response.json() #dict 리턴됨
+
+    #1등 번호 6개가 담긴 result라는 list 출력    
+    for i in range(1,7):
+        winner.append(res_dict['drwtNo'+ str(i)])
 
     #만약에 6개가 모두 일치하면 1등
     #만약 5개가 일치하면 3등
